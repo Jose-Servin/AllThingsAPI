39d08a14d3208581227d167c5789e42a711a5ea4
Jose Servin <Servinjose96@gmail.com>
2022-11-22 14:56:57 -0600

    starting DOM

---
+++ HTML_CSS/WebDesign/PyWebDev/Guess-My-Number/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// returns back the p element text
+console.log(document.querySelector(".message").textContent)
+
+// select score and change the element
+document.querySelector(".score").textContent = 10000
+
+// send value to input field 
+document.querySelector(".guess").value = 50;
+
+// handling click events
