7097d997f9cb397564d429336e9c60a4c8f08bc5
Jose Servin <Servinjose96@gmail.com>
2022-11-23 08:47:47 -0600

    developing guess my number game logic in javascript

--- HTML_CSS/WebDesign/PyWebDev/Guess-My-Number/script.js
+++ HTML_CSS/WebDesign/PyWebDev/Guess-My-Number/script.js
@@ -1,12 +1,27 @@
 'use strict';
 
-// returns back the p element text
-console.log(document.querySelector(".message").textContent)
+// defining secret number outside our event listener 
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
 
-// select score and change the element
-document.querySelector(".score").textContent = 10000
+const random_number = getRandomIntInclusive(1, 20);
+console.log(random_number)
 
-// send value to input field 
-document.querySelector(".guess").value = 50;
+document.querySelector(".check").addEventListener(
+    'click',
+    function () {
+        // first console log value of input field 
+        const guess = Number(document.querySelector(".guess").value);
+        console.log(typeof guess);
 
-// handling click events
+        // if no number entered guess is False, so we want this condition to be true to run. 
+        // here we separate boolean value vs condition boolean 
+        if (!guess) {
+            document.querySelector(".message").textContent = "No Number!"
+        }
+
+    }
+)
