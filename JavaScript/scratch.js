aa99d21b6353bbad40f6cf10b0fbcadb319ec7c7
Jose Servin <Servinjose96@gmail.com>
2023-03-09 16:27:16 -0600

    updating main

--- JavaScript/scratch.js
+++ JavaScript/scratch.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+console.log("Hello World");
+const Book = {
+    topic: "Python",
+    content: {
+        ch1: "Intro",
+        ch2: "Variables",
+        ch3: "Logic"
+    }
+};
+
+let foo = Book?.content?.ch1;
+console.log(foo)
