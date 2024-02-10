5cb35e4a225202b90d15e072382905ae1b6af194
Jose Servin <Servinjose96@gmail.com>
2024-02-10 11:32:22 -0600

    Notes: JavaScript modal window notes

--- Projects/ModalWindow/script.js
+++ Projects/ModalWindow/script.js
@@ -1,36 +1,47 @@
-'use strict'
+'use strict';
 
 // Begin by selecting the DOM elements
-const modalElm = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
+const modalElement = document.querySelector(".modal")
+const overlayBackground = document.querySelector(".overlay");
 const closeModalBtn = document.querySelector(".close-modal");
+// here we use querySelectorAll to find ALL Show Modal buttons
+// This returns a node list we can iterate through.
 const showModalBtn = document.querySelectorAll(".show-modal");
 
-// EvenFunctions
+// EventFunctions
+// If we want to "close" something, we add the hidden class
 function closeModal() {
-    modalElm.classList.add("hidden");
-    overlay.classList.add("hidden");
+    modalElement.classList.add("hidden");
+    overlayBackground.classList.add("hidden");
 }
 
+// If we want to "open" something, we remove the hidden class.
 function openModal() {
-    modalElm.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+    // Show the modal div
+    modalElement.classList.remove("hidden");
+    // Show the overlay dark blurry background
+    overlayBackground.classList.remove("hidden");
 }
 
-
-// Adding eventListener to each modalBtn
+// Adding eventListener to each button in our showModalBtn node list
 for (let i = 0; i < showModalBtn.length; i++) {
     showModalBtn[i].addEventListener('click', openModal);
 };
-// not dependent on Modal buttons so handled outside of for loop
+
+// not dependent on showModalBtn buttons so handled outside of for loop
+// Once a modal window opens, there will ALWAYS only be just 1 close button.
 // handling closeModalBtn
 closeModalBtn.addEventListener('click', closeModal)
-// handling clicking overlay to close
-overlay.addEventListener('click', closeModal)
+// handling clicking overlayBackground to close modal window
+overlayBackground.addEventListener('click', closeModal)
 
-// escape key handling
+// Global events like key clicking occur on the document
+// here we have to check two conditions
+// Check 1 - Escape key was pressed (check the evt object for this)
+// Check 2 - The modal window is currently shown i.e It does NOT contain the hidden class (check classList for this)
+// The information of each event is stored in the evt object and is accessible in our eventHandler function.
 document.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Escape' && !modalElm.classList.contains("hidden")) {
+    if (evt.key === 'Escape' && !modalElement.classList.contains("hidden")) {
         closeModal();
     }
 });
